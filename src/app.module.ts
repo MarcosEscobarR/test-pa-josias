57f2e98316b99c90561a4653562c3e70e62995f6
MarcosEscobarR <marcosmaer600@gmail.com>
2022-05-24 10:49:22 -0400

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TaskModule } from './task/task.module';
+import ormconfig from './ormconfig';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, TaskModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...ormconfig,
+      keepConnectionAlive: true,
+      autoLoadEntities: true,
+    }),
+    UsersModule,
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
